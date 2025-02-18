e9562b96c5685925739d2b903386a37dfc0271c7
Joseph Lau <lau.joseph.ca@gmail.com>
2025-02-18 17:32:57 +0800

    commit

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import Nav from "./Nav"
+
+export default function App() {
+    return (
+        <>
+            <h1>react</h1>
+            <Nav />
+        </>
+    )
+}
